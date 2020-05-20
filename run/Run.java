243c964022669c4cd323bdd6394a4688804cb590
Duhan Mo <devduhanmo@gmail.com>
2020-05-20 17:12:11 +0900

    run src

---
+++ run/Run.java
@@ -0,0 +1,20 @@
+package com.kh.practice.run;
+
+import com.kh.practice.*;
+
+public class Run {
+    public static void main(String[] args){
+
+        ArrayPractice a = new ArrayPractice();
+//        a.method1();
+//        a.method2();
+//        a.method3();
+        a.method4();
+//        a.method5();
+//        a.method6();
+
+
+
+
+    }
+}
