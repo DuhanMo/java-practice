39e09b0ca420900da7f7f5a9c67fd237f38e7ba8
Duhan Mo <devduhanmo@gmail.com>
2020-05-24 16:23:43 +0900

    20200524 field practice2

--- Member05.java
+++ Member05.java
@@ -2,26 +2,28 @@ package com.kh.practice;
 
 
 class InitBlock2 {
-    static int classVar = 10;
-    int instaceVar = 10;
+    static int classVar = 10; // 클래스 변수의 명시적 초기화
+    int instanceVar = 10; // 인스턴스 변수의 명시적 초기화
 
     static {
-        classVar = 20;
+        classVar = 20; // 클래스 초기화 블록을 이용한 초기화
     }
 
     {
-        instaceVar = 20;
+        instanceVar = 20; // 인스턴스 초기화 블록을 이용한 초기화
     }
 
-//    InitBlock.classVar = 500;
+    InitBlock2() {
+        instanceVar = 30;
+    } // 생성자를 이용한 초기화
+
 }
 
 public class Member05 {
     public static void main(String[] args) {
-        InitBlock.classVar = 5000;
-        System.out.println(InitBlock.classVar);
         System.out.println(InitBlock2.classVar);
-
+        InitBlock2 myInit = new InitBlock2();
+        System.out.println(myInit.instanceVar);
     }
 
 }
