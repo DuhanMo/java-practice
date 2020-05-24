bdfdbd8fa66ade7beea9d0ea7ef612c38cc37b9f
Duhan Mo <devduhanmo@gmail.com>
2020-05-24 14:31:00 +0900

    20200524

--- CommitTest.java
+++
@@ -1,6 +0,0 @@
-package com.kh.practice;
-
-public class CommitTest {
-
-    //git commit test Add line
-}
--- run/Run.java
+++ run/Run.java
@@ -9,10 +9,19 @@ public class Run {
 //        a.method1();
 //        a.method2();
 //        a.method3();
-        a.method4();
+//        a.method4();
 //        a.method5();
 //        a.method6();
 
+        DimentionalArray b = new DimentionalArray();
+//        b.method1();
+//        b.method2();
+//        b.method3();
+//        b.method4();
+//        b.method5();
+//        b.method6_v1();
+//        b.method6_v2(); // 내 코드, 직접적으로 배열에 저장을 하다보니 범위넘어가는 행,열 입력시 바운더리 오류 발생
+//        b.method7();
 
 
 
