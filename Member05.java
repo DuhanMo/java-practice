3fffc28379e76dab910848f6dae23318ac756e78
Duhan Mo <devduhanmo@gmail.com>
2020-05-25 19:37:57 +0900

    20200524

--- Member05.java
+++ Member05.java
@@ -1,6 +1,7 @@
 package com.kh.practice;
 
 
+
 class InitBlock2 {
     static int classVar = 10; // 클래스 변수의 명시적 초기화
     int instanceVar = 10; // 인스턴스 변수의 명시적 초기화
@@ -13,17 +14,13 @@ class InitBlock2 {
         instanceVar = 20; // 인스턴스 초기화 블록을 이용한 초기화
     }
 
-    InitBlock2() {
-        instanceVar = 30;
-    } // 생성자를 이용한 초기화
+    {InitBlock.classVar = 123123;}
 
 }
 
 public class Member05 {
     public static void main(String[] args) {
-        System.out.println(InitBlock2.classVar);
-        InitBlock2 myInit = new InitBlock2();
-        System.out.println(myInit.instanceVar);
+        System.out.println(InitBlock.classVar);
     }
 
 }
